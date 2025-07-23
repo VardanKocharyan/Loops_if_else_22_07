64d9a0db3aafbed0b68944c262946ebebf1e3514
VardanKocharyan <vardanqocharyan5>
2025-07-23 14:02:30 +0400

    update

--- calculat.c
+++ calculat.c
@@ -4,8 +4,6 @@ int main(){
 
 	int num1 = 0;
 	int num2 = 0;
-	float res = 0;
-
 	char op = '\0';
 
 	printf("Enter the firs num:");
@@ -16,7 +14,7 @@ int main(){
 		printf("Enter the second one:");
 		scanf("%d" , &num2);
 
-		printf("Enter the firs num:");
+		printf("Enter the operation:");
 		scanf(" %c" , &op);
 
 		switch (op) {
@@ -32,11 +30,14 @@ int main(){
 			case '/':
 				printf("%d / %d = %d\n" , num1 , num2 , num1 / num2);
 			break;
+			default:
+				printf("Unknown opertion\n");
 }
 
 	printf("Enter the firs num:");
         printf("(q to quit):");
 }
 
+	printf("Exiting\n");
 return 0;
 }
